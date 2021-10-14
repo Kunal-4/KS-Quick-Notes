04f975c02b279b07cb0676e680372c8889fbe805
kunal-4 <kunalshaw195@gmail.com>
2021-10-14 19:24:48 +0530

    Delete function not implemented, some errors to fix

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int main(){     
+    vector<int> v;
+    v.push_back(1);  
+    v.push_back(2);
+    v.push_back(3); 
+    for(int i=0 ; i < v.size(); i++) cout<<v[i]<<"  ";
+
+    for(int i=v.begin(); i < v.end(); i++) cout<<v[i]<<"  ";
+    return 0;
+}
